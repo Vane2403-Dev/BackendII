8355b9198faf761be52156ff46585738dc023e4e
vcpvanedev <vcp2403@gmail.com>
2025-04-26 09:13:46 -0300

    validaciones completas

---
+++ proyectoFinal/src/app.js
@@ -0,0 +1,46 @@
+import express from "express";
+import { connectMongoDB } from "./config/mongoDB.config.js";
+import session from "express-session";  
+import routes from "./routes/index.js";
+import envsConfig from "./config/env.config.js";
+import cookieParser from "cookie-parser";
+import passport from "./config/passport/passport.config.js";
+
+
+const app = express();
+
+connectMongoDB();
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+
+app.use(
+  session({
+    secret: envsConfig.SESSION_SECRET,
+    resave: true, // Evita guardar la sesión si no hay cambios
+    saveUninitialized: true, // Guarda sesiones vacías
+    cookie: { secure: false, maxAge: 500000 }, // Debe estar en true si usas HTTPS
+  })
+);
+
+app.use(cookieParser());
+
+app.use(passport.initialize());
+
+// Rutas de la api
+app.use("/api", routes);
+
+// Ruta para manejar errores 404
+app.use((req, res) => {
+  res.status(404).json({ error: "Recurso no encontrado" });
+});
+
+app.listen(envsConfig.PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${envsConfig.PORT}`);
+});
