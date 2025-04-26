cab48c34d5b344fd3610a41cfc83494a278944eb
vcpvanedev <vcp2403@gmail.com>
2025-04-26 11:00:01 -0300

    cart controllers

--- proyectoFinal/src/routes/carts.routes.js
+++ proyectoFinal/src/routes/carts.routes.js
@@ -1,98 +1,20 @@
 import { Router } from "express";
-import { cartDao } from "../persistence/mongo/dao/cart.dao.js";
+import { cartController } from "../controllers/cart.controller.js";
 
 
 
 const router = Router();
 
-router.post("/", async (req, res) => {
-  try {
-    const cart = await cartDao.create();
+router.post("/", cartController.createCart);
 
-    res.status(201).json({ status: "ok", cart });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ status: "Erro", msg: "Error interno del servidor" });
-  }
-});
+router.get("/:cid", cartController.getById);
 
-router.get("/:cid", async (req, res) => {
-  try {
-    const { cid } = req.params;
-    const cart = await cartDao.getById(cid);
-    if (!cart) return res.status(404).json({ status: "Error", msg: "Carrito no encontrado" });
+router.post("/:cid/product/:pid", cartController.addProductToCart);
 
-    res.status(200).json({ status: "ok", cart });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ status: "Erro", msg: "Error interno del servidor" });
-  }
-});
+router.delete("/:cid/product/:pid", cartController.deleteProductsToCart);
 
-router.post("/:cid/product/:pid", async (req, res) => {
-  try {
-    const { cid, pid } = req.params;
-    const product = await productDao.getById(pid);
-    if (!product) return res.status(404).json({ status: "Error", msg: `No se encontró el producto con el id ${pid}` });
-    const cart = await cartDao.getById(cid);
-    if (!cart) return res.status(404).json({ status: "Error", msg: `No se encontró el carrito con el id ${cid}` });
+router.put("/:cid/product/:pid", cartController.updateQuantityProductInCart);
 
-    const cartUpdate = await cartDao.addProductToCart(cid, pid);
-
-    res.status(200).json({ status: "ok", payload: cartUpdate });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ status: "Erro", msg: "Error interno del servidor" });
-  }
-});
-
-router.delete("/:cid/product/:pid", async (req, res) => {
-  try {
-    const { cid, pid } = req.params;
-    const product = await productDao.getById(pid);
-    if (!product) return res.status(404).json({ status: "Error", msg: `No se encontró el producto con el id ${pid}` });
-    const cart = await cartDao.getById(cid);
-    if (!cart) return res.status(404).json({ status: "Error", msg: `No se encontró el carrito con el id ${cid}` });
-
-    const cartUpdate = await cartDao.deleteProductToCart(cid, pid);
-
-    res.status(200).json({ status: "ok", payload: cartUpdate });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ status: "Erro", msg: "Error interno del servidor" });
-  }
-});
-
-router.put("/:cid/product/:pid", async (req, res) => {
-  try {
-    const { cid, pid } = req.params;
-    const { quantity } = req.body;
-
-    const product = await productDao.getById(pid);
-    if (!product) return res.status(404).json({ status: "Error", msg: `No se encontró el producto con el id ${pid}` });
-    const cart = await cartDao.getById(cid);
-    if (!cart) return res.status(404).json({ status: "Error", msg: `No se encontró el carrito con el id ${cid}` });
-
-    const cartUpdate = await cartDao.updateQuantityProductInCart(cid, pid, Number(quantity));
-
-    res.status(200).json({ status: "ok", payload: cartUpdate });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ status: "Erro", msg: "Error interno del servidor" });
-  }
-});
-
-router.delete("/:cid", async (req, res) => {
-  try {
-    const { cid } = req.params;
-    const cart = await cartDao.clearProductsToCart(cid);
-    if (!cart) return res.status(404).json({ status: "Error", msg: "Carrito no encontrado" });
-
-    res.status(200).json({ status: "ok", cart });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ status: "Erro", msg: "Error interno del servidor" });
-  }
-});
+router.delete("/:cid", cartController.clearProductsToCart);
 
 export default router;
